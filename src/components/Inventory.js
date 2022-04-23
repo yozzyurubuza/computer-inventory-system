95e46e5ef4d9f2cd2e4536e4ab5e9a1039d5e825
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-23 20:41:45 +0800

    Add table scrolling

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { INVENTORY_LIST } from "../config";
+import "../css/Inventory.css";
 
 const Inventory = () => {
-  const generateInventory = INVENTORY_LIST.map((item) => {
+  const generateInventory = INVENTORY_LIST.map((item, i) => {
     return (
-      <tr>
+      <tr key={"I_" + i}>
         <td>{item.item_id}</td>
         <td>{item.item_image}</td>
         <td>{item.item_name}</td>
-        <td class="right aligned">{item.quantity}</td>
+        <td className="right aligned">{item.quantity}</td>
       </tr>
     );
   });
 
   return (
-    <div>
-      <table class="ui unstackable table">
+    <div style={{ maxHeight: "200px", overflowY: "scroll" }}>
+      <table className="ui unstackable table">
         <thead>
           <tr>
             <th>Item ID</th>
             <th>Image</th>
             <th>Item Name</th>
-            <th class="right aligned">Quantity</th>
+            <th className="right aligned">Quantity</th>
           </tr>
         </thead>
         <tbody>{generateInventory}</tbody>
